865a93d52a1babd3e18466f9434753963374b443
Diego Pereira <dipereir@redhat.com>
2018-08-30 13:55:23 -0300

    Attempt to make responses work with jenkins

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -83,32 +83,32 @@ def no_local_folders():
         shutil.rmtree(_dir, ignore_errors=True)
 
 
+@responses.activate
 @pytest.fixture
 def influx_db_mock():
-    with responses.activate():
-        # responses.reset()
-        mnm.INFLUXDB_PLATFORM = 'http://some.influx.endpoint.com/write?db=platform'
-        responses.add(
-            responses.POST, mnm.INFLUXDB_PLATFORM,
-            json={"message": "saved"}, status=201
-        )
+    # responses.reset()
+    mnm.INFLUXDB_PLATFORM = 'http://some.influx.endpoint.com/write?db=platform'
+    responses.add(
+        responses.POST, mnm.INFLUXDB_PLATFORM,
+        json={"message": "saved"}, status=201
+    )
 
-        yield mnm.INFLUXDB_PLATFORM
-        mnm.INFLUXDB_PLATFORM = os.getenv('INFLUXDB_PLATFORM')
+    yield mnm.INFLUXDB_PLATFORM
+    mnm.INFLUXDB_PLATFORM = os.getenv('INFLUXDB_PLATFORM')
 
 
+@responses.activate
 @pytest.fixture
 def influx_db_error_mock():
-    with responses.activate():
-        # responses.reset()
-        mnm.INFLUXDB_PLATFORM = 'http://some.influx.endpoint.com/write?db=platform'
-        responses.add(
-            responses.POST, mnm.INFLUXDB_PLATFORM,
-            json={"message": 'error'}, status=422
-        )
+    # responses.reset()
+    mnm.INFLUXDB_PLATFORM = 'http://some.influx.endpoint.com/write?db=platform'
+    responses.add(
+        responses.POST, mnm.INFLUXDB_PLATFORM,
+        json={"message": 'error'}, status=422
+    )
 
-        yield mnm.INFLUXDB_PLATFORM
-        mnm.INFLUXDB_PLATFORM = os.getenv('INFLUXDB_PLATFORM')
+    yield mnm.INFLUXDB_PLATFORM
+    mnm.INFLUXDB_PLATFORM = os.getenv('INFLUXDB_PLATFORM')
 
 
 @pytest.fixture
